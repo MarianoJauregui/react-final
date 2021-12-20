51eaeb6cfd44e753e21111d327e09bf924e1c3f6
Mariano Jáuregui <marian_jauregui@hotmail.com>
2021-12-20 13:28:37 -0300

    Logo agregado

--- Ecommerce-main/src/components/Quienes/Quienes.jsx
+++ Ecommerce-main/src/components/Quienes/Quienes.jsx
@@ -5,6 +5,7 @@ const Quienes = () => {
     return(
         <section className="quienes">
             <p> <span>MarAnd's</span> es una tienda de instrumentos musicales de primera calidad con 4 sedes en la provincia de Buenos Aires</p>
+            <img src="https://marandsmusic.com/wp-content/uploads/2021/03/logo-marands.png" alt="quienes" width="100%" />
         </section>
         
     )
